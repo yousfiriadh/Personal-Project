8b5404c8b82d3d5a58e03d31e65980403b741360
yousfiriadh <yousfiriadh69@gmail.com>
2021-11-25 21:06:20 +0100

    commit

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -7,6 +7,8 @@ import { FooterComponent } from './footer/footer.component';
 import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   imports: [
@@ -22,6 +24,10 @@ import { ExperienceComponent } from './experience/experience.component';
     AboutComponent,
   
     ExperienceComponent,
+  
+    ContactComponent,
+  
+    EducationComponent,
 
   ],
   providers: [
